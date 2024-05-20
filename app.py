ce4bcd5fa3b817ea3b4741b2223ee41c268b42fd
FaithM <faith.muthoni@assentcompliance.com>
2024-05-20 19:12:14 +0300

    working with Teklado on Flask

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 posts = {
     0: {
+        'id': 0,
         'title': 'Hello, World',
         'content': 'This is my first blog post!'
     }
@@ -11,7 +12,7 @@ posts = {
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('home.jinja2',posts=posts )
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
@@ -21,6 +22,16 @@ def post(post_id):
     return render_template('post.jinja2',post=post)
 
 
+@app.route('/post/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_id = len(posts)
+        posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+
+        return redirect(url_for('post', post_id=post_id))
+    return render_template('create.jinja2')
 
 if __name__ == '__main__':
     app.run(debug=True)
